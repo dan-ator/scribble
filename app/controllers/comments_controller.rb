d2ff32b979a8e10b9c55749b485d9a622e44db80
Daniel Alexander <daniel.elliot.alexander@gmail.com>
2015-11-12 13:38:35 -0500

    added full crud for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,7 +14,7 @@ class CommentsController < ApplicationController
   #create
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(song_params)
+    @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
   end
 
@@ -26,24 +26,27 @@ class CommentsController < ApplicationController
 
   #edit
   def edit
-    @comment = Comment.find(params[:id])
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
   end
 
   #update
   def update
-    @comment = Comment.find(params[:id])
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id]).update(comment_params)
     redirect_to post_path(@post)
   end
-  #
-  # #destroy
-  # def destroy
-  #   @post = Post.find(params[:id])
-  #   @post.destroy
-  #   redirect_to posts_path
-  # end
+
+  #destroy
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
 
   private
-  def song_params
+  def comment_params
       params.require(:comment).permit(:title, :body, :author)
   end
 
